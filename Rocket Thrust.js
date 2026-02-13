1ffcaf9a85862ac6f43c8cd646b32b2c88ab8ed8
JustAndrei <mugman121309@gmail.com>
2026-02-13 13:34:45 -0500

    move now yay

--- Rocket Thrust.js
+++ Rocket Thrust.js
@@ -1,6 +1,21 @@
 function Thrust(){
-if (keyCode === 'w' || keyCode === 'W') {
-	var vel = createVector(0, -5).rotate(Angle)
-	rocketPos.add(vel)
-}
+var vertMove = false
+var horiMove = false
+if (keyIsPressed === true) {
+	if (keyCode === 87||keyCode === 87) {
+	vertMove = true
+	}
+}	else if (keyCode === 83||keyCode === 83) {
+	vertMove = false
+	}
+
+var speedy = createVector(0, -5).rotate(Angle)
+var speedx = createVector(5, 0).rotate(Angle)
+
+	if (vertMove === true) {
+	rocketPos.add(speedy)
+	print(vertMove)
+	}
+
+
 }
